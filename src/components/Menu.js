8419eaa0868ff0b0cebf3b22fcd88a68d9374aec
Lis Setgasy <Lis@collplant-ltd.local>
2022-07-21 15:04:49 +0300

    started project

---
+++ src/components/Menu.js
@@ -0,0 +1,21 @@
+import { Navbar } from "react-bulma-companion";
+
+const Menu = () =>{
+    return (
+        <Navbar>
+            <Navbar.Brand>
+                
+            <Navbar.Item>Products</Navbar.Item>
+            <Navbar.Item>Customers</Navbar.Item>
+            <Navbar.Item>Purchases</Navbar.Item>
+          
+            </Navbar.Brand>
+
+           
+        </Navbar>
+    )
+
+}
+
+
+export default Menu;
